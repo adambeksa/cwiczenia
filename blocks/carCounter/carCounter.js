94fb17d0bffe3dd9a505c4f9c71ea8b23df15184
Adam Beksa <abeksa@softfusion.pl>
2016-12-15 16:26:52 +0100

    CarCounter end

--- blocks/carCounter/carCounter.js
+++ blocks/carCounter/carCounter.js
@@ -1,19 +1,51 @@
+var $tip = $('.carCounter__tipCircle');
+
+$(document).ready(function () {
+  time();
+  setTimeout(function () {
+    $tip.addClass('animation')
+  },1500);
+  setTimeout(function () {
+    $tip.removeClass('animation');
+    $('.-icon').css('opacity','0')
+  },3000);
+});
 
 $(document).keydown(function(e) {
-  var $tip = $('.tipCircle'),
-      $deg = 0;
+  $('.-icon').css('opacity','1');
   switch(e.which) {
-
     case 38: // up
-      $deg += 10;
-      alert($deg)
-      $tip.css({
-          transform: "translate(-50%,-50%) rotate("+$deg+"deg)"
-      });
+      if(! $tip.hasClass('animation')){
+        $tip.addClass('animation');
+        setTimeout(function () {
+          $tip.removeClass('animation');
+          $('.-icon').css('opacity','0')
+        },3000);
+      }
       break;
 
-    case 40: // down
+    case 87: // down
+      if(! $tip.hasClass('animation')){
+        $tip.addClass('animation');
+        setTimeout(function () {
+          $tip.removeClass('animation');
+          $('.-icon').css('opacity','0')
+        },3000);
+      }
       break;
 
   }
 });
+
+function time() {
+  function leadingZero(i) {
+    return (i < 10) ? '0' + i : i;
+  }
+  var currentdate = new Date();
+  var datetime = + leadingZero(currentdate.getHours()) + ":" + leadingZero(currentdate.getMinutes()) ;
+  $('.carCounter__time').text(datetime)
+
+  setTimeout(function () {
+    time()
+  },1000)
+}
